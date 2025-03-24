d8bd6447675c2f4635ba4c71e514001dccad1c31
gabrielbarrera0305 <gabrielbarrera0305@gmail.com>
2025-03-24 08:19:24 -0400

    Add files via upload

---
+++ sumar numeros pares del 1 al 100 ejercicio 6.js
@@ -0,0 +1,5 @@
+num = 0;
+
+for(let i = 0; i <= 100; i++) {
+    console.log(i++)
+}
---
+++ trabla del multiplicar ejercicio 7.js
@@ -0,0 +1,6 @@
+num = prompt("Digite un numero");
+
+for(let i = 1; i < 11; i++) {
+    mul = num * i;
+    console.log(${num} x ${i} = ${mul});
+}
