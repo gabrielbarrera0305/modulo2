12a3aff27e47b6fe296f1fa2e6266a622f49207a
gabrielbarrera0305 <gabrielbarrera0305@gmail.com>
2025-03-23 22:16:36 -0400

    Add files via upload

---
+++ ejecicio 1.js.js
@@ -0,0 +1,13 @@
+var nombre1 = "nuritzia";
+let  edad1 = 25;
+const ciudad ="caracas";
+
+console.log(ciudad)
+console.log(edad1)
+console.log(nombre1)
+
+let nombre2 = "daniel"; 
+let clase = ("programacion",2) ; 
+let año = 2025 ; 
+let edad2 = "tengo "+ 24 +" años"
+console.log(edad2)
