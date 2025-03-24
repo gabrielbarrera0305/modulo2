2f48313cf84bdbe40d0a5a40884e9e45f3cf7894
gabrielbarrera0305 <gabrielbarrera0305@gmail.com>
2025-03-24 00:12:18 -0400

    Delete temperaruta c a k ejercicio 6.php

--- temperaruta c a k ejercicio 6.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Ingresa la temperatura en grados Celsius: ";
-$celsius = (float)trim(fgets(STDIN)); 
-
-$fahrenheit = ($celsius * 9/5) + 32;
-
-echo "$celsius °C equivale a $fahrenheit °F\n";
-?>
