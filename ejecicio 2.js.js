640fe5f4b752fced258aae1b30f7c5063b1f3e84
gabrielbarrera0305 <gabrielbarrera0305@gmail.com>
2025-03-23 22:18:37 -0400

    Add files via upload

---
+++ ejecicio 2.js.js
@@ -0,0 +1,8 @@
+var edad7 = 39;
+if (edad7 < 40) {
+    console.log("es menor de edad");
+} else if (edad7 >= 40 && edad7 < 65) {
+    console.log("es mayor de edad");
+} else {
+    console.log("o tiene menos de 1 años o ya vas a la funeraria")
+}
